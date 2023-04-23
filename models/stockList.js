f06eefc4a46a1086330ddd9731242cd70cf2edee
AJ Glodowski <ajglodo@gmail.com>
2023-04-23 16:32:26 -0400

    Fixing list model stuff

--- models/stockList.js
+++ models/stockList.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
+const User = require('../models/user');
+const Stock = require('../models/stock');
 
 const StockListSchema = mongoose.Schema({
     list_name: {
         type: mongoose.Schema.Types.String,
         required: true
     },
-    stocks: {
-        type: [mongoose.Schema.Types.ObjectId],
-        ref: 'stocks',
+    list_owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User.modelName,
         required: true
-    }
+    },
+    stocks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Stock.modelName,
+    }]
 });
 
 module.exports = mongoose.model('StockList', StockListSchema, "stocklists");
