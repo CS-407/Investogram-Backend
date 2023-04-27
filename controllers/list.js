959e6caf2cd2cef9d8bf2f54479ab9dd5feb486e
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2023-04-27 16:03:04 -0400

    added endpoint to create new list

--- controllers/list.js
+++ controllers/list.js
@@ -1,5 +1,41 @@
 const StockList = require('../models/stockList');
 
+exports.getLists = async (req, res) => {
+    try {
+        const lists = await StockList.find({ list_owner: req.user.id }).populate('stocks');
+
+        res.status(200).json({ msg: "Success", lists: lists });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server Error' });
+    }
+}
+
+exports.createNewList = async (req, res) => {
+    try {
+        const { listName } = req.body;
+
+        const existing_list = await StockList.findOne({ list_name: listName, list_owner: req.user.id });
+
+        if (existing_list) {
+            return res.status(400).json({ msg: "List already exists" });
+        }
+
+        const newList = new StockList({
+            list_name: listName,
+            list_owner: req.user.id,
+            stocks: []
+        });
+
+        const new_list = await newList.save();
+
+        res.status(200).json({ msg: "Success", list: new_list });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server Error' });
+    }
+}
+
 exports.getList = async (req, res) => {
     try {
         const { listId } = req.params;
--- routes/list.js
+++ routes/list.js
@@ -4,10 +4,16 @@ const router = express.Router();
 const listController = require("../controllers/list");
 const isAuth = require('../middleware/isAuth');
 
-// Get a list
-router.get("/get/:listId", listController.getList);
+// Get user's lists
+router.get("/", isAuth, listController.getLists);
+
+// Create a new list
+router.post("/create", isAuth, listController.createNewList);
 
 // Get a list
+router.get("/get/:listId", isAuth, listController.getList);
+
+// Update a list
 router.post("/update", listController.updateList);
 
 module.exports = router;
